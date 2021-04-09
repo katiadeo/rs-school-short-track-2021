caa227c3360f115827d3c6e15a7fcc83080317bf
katiadeo <niel.0305@yandex.ru>
2021-04-09 21:49:31 +0300

    feat: start matrix-elements-sum task

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,20 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      // sum up all digits of the zero array in matrix as there are no zeros above it
+      if (i === 0) {
+        result = matrix[i].reduce((acc, current) => acc + current, 0);
+        // if the characer of uppper array in matrix is not zezo, sum up this character
+      } else if (matrix[i - 1][j] !== 0) {
+        result += matrix[i][j];
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = getMatrixElementsSum;
