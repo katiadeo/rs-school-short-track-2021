b42aa9d19b003b515584bf5dea3b6235b4c827ef
katiadeo <niel.0305@yandex.ru>
2021-04-09 17:37:40 +0300

    feat: start sum-digits task

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,14 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  // convert number into an array of single digits
+  const arr = Array.from(String(n), Number);
+  // sum the digits once
+  const sum = arr.reduce((acc, current) => acc + current, 0);
+  // as 9 is the biggest number with 1 digit -> check if the sum is more than 9
+  // use recursion
+  return (sum >= 10) ? getSumOfDigits(sum) : sum;
 }
 
 module.exports = getSumOfDigits;
