2b9d05438ef24b6360854c6a3c25f1043596cb4f
katiadeo <niel.0305@yandex.ru>
2021-04-10 17:55:30 +0300

    feat: start encode-line task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,36 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const result = [];
+  const arr = str.split('');
+  let counter = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] === arr[i]) {
+      counter++;
+    } else if (counter === 1) {
+      result.push(arr[i]);
+    } else {
+      result.push(counter + arr[i]);
+      counter = 1;
+    }
+  }
+
+  return result.join('');
 }
 
 module.exports = encodeLine;
+
+// create an object with counts {a: 1, b: 3 ... }
+// const counts = arr.reduce((acc, curr) => {
+//   acc[curr] = acc[curr] ? acc[curr] + 1 : 1;
+//   return acc;
+// }, {});
+
+// Object.entries(counts).forEach(([key, value]) => {
+//   if (value === 1) result.push(`${key}`);
+//   else result.push(`${value}${key}`);
+// });
+
+// return result.join('');
